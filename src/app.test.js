5efc5cb4f0cddb115812b2d964886fcd1be8cb53
smazzone <stefano.mazzone@gmail.com>
2025-03-03 15:47:28 +0100

    change message

--- src/app.test.js
+++ src/app.test.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('It should respond with "Hello CI/CD! from ...{somewhere}"', async () => {
+  it('It should respond with "Hello CI/CD from ...{somewhere}"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toContain('Hello CI/CD! from');
+    expect(response.text).toContain('Hello CI/CD from');
   });
 });
