f49f8be6ade4d8e07b2a78184506b5ee3b050c1c
smazzone <stefano.mazzone@gmail.com>
2025-06-10 10:39:49 +0200

    update the message printed

--- src/app.test.js
+++ src/app.test.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('It should respond with "Hello CI/CD and some other info..."', async () => {
+  it('It should respond with "Hello from CI/CD and some other info..."', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toContain('Hello CI/CD');
