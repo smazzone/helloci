763a019024b8af45da81f69305f9632aa686ae0c
Stefano Mazzone <smazzone@users.noreply.github.com>
2025-07-22 14:18:33 +0200

    Update app.js
    removed leaks

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 const os = require("os");
-const LEAKED_API_KEY = "b780002b85d6411ca0ad9f9c60195f72";
-const PASSWORD = "b780002b85d6411ca0ad9f9c60195f72" 
-const TOKEN = "b780002b85d6411ca0ad9f9c60195f72"
 
 app.get('/', (req, res) => {
   const appVersion = process.env.APP_VERSION || '';
