bf1a85c390f77d65975c3dce8c97656b69fd21fd
smazzone <stefano.mazzone@gmail.com>
2025-07-22 14:30:15 +0200

    latest

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const os = require("os");
 
+
 app.get('/', (req, res) => {
   const appVersion = process.env.APP_VERSION || '';
   const message = appVersion !== '' ? `Hello from CI/CD - ${os.hostname()} - Version: ${appVersion}` : `Hello from CI/CD - ${os.hostname()}`;
