f129dc6f25118864a5c04de198430a6d2344e8ff
smazzone <stefano.mazzone@gmail.com>
2025-07-22 11:41:05 +0200

    inject a leaked secret

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const os = require("os");
-
+const LEAKED_API_KEY = "your-super-secret-api-key-leaked"; 
 
 app.get('/', (req, res) => {
   const appVersion = process.env.APP_VERSION || '';
